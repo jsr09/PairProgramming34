9a2379ecb10b127ab544056ab4648018fe22fa95
Christopher Soriano <115194701+cmsoriano1022@users.noreply.github.com>
2022-12-14 12:29:11 -0800

    Updated PairProgramming34

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+//EXERCISE ONE
+const myButton = document.querySelector("#cookie");
 
+myButton.addEventListener('click', function() {
+console.log('Eat Me!');
+counter = counter +1;
+
+const myCounter = document.querySelector('#counter');
+myCounter.innerHTML = counter;
+
+})
+let counter = 0;
+
+// EXERCISE TWO
